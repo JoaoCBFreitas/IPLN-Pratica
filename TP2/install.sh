1a33cbc8a0ef5ffc2baae39d47118121d8dd5743
João Freitas <36702747+JoaoCBFreitas@users.noreply.github.com>
2021-01-09 21:06:26 +0000

    Update install.sh

--- TP2/install.sh
+++ TP2/install.sh
@@ -1,6 +1,7 @@
 sudo cp rimapalavra /bin/
 sudo chmod 777 /bin/rimapalavra
 sudo pip3 install numpy
+sudo pip3 install Cython
 sudo pip3 install spacy
 sudo pip3 install tqdm
 python3 -m spacy download pt_core_news_lg
