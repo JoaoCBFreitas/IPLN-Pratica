372f832605ca25978820f3ae8f2887678d39da5e
João Freitas <36702747+JoaoCBFreitas@users.noreply.github.com>
2021-01-09 21:21:45 +0000

    Módulo requests

--- TP2/install.sh
+++ TP2/install.sh
@@ -6,5 +6,6 @@ sudo pip3 install Cython
 sudo pip3 install spacy
 sudo pip3 install tqdm
 sudo pip3 install bs4
+sudo pip3 install requests
 python3 -m spacy download pt_core_news_lg
 rimapalavra -h
